b8d420c58494a8a0e01c5ef3b54a3e506bdb5606
Aleksey <krugombeda@gmail.com>
2020-11-04 12:29:08 +0300

    extract nav to component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
-import styles from './Header.module.scss';
 import Logo from '../../icons/logo.svg';
+import Navbar from '../Navbar/Navbar';
+import styles from './Header.module.scss';
 
 export interface HeaderProps {}
 
@@ -10,30 +10,7 @@ const Header: React.FC<HeaderProps> = () => {
     <header className={styles.header}>
       <div className="container flex-row">
         <Logo />
-        <nav>
-          <ul className={styles.menu}>
-            <li>
-              <a href="#" className={styles.menuItem}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.menuItem}>
-                Pokédex
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.menuItem}>
-                Legendaries
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.menuItem}>
-                Documentation
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
       </div>
     </header>
   );
