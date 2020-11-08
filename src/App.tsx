52c77a440067f87b803dbb67b200c2f776e2768f
Aleksey <krugombeda@gmail.com>
2020-11-08 20:57:25 +0300

    add router, crete pokedex page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import PokedexPage from './pages/Pokedex/Pokedex';
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/pokedex" component={PokedexPage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import Button from '../../components/Button/Button';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import Button from '../../components/Button/Button';
-import Parallax from '../../components/Parallax/Parallax';
-import Layout from '../../components/Layout/Layout';
 import Heading from '../../components/Heading/Heading';
+import Layout from '../../components/Layout/Layout';
+import Parallax from '../../components/Parallax/Parallax';
 
 import styles from './HomePage.module.scss';
 
-export interface HomePageProps {}
+export interface HomePageProps extends RouteComponentProps {}
 
-const HomePage: React.FC<HomePageProps> = () => {
+const HomePage: React.FC<HomePageProps> = ({ history: { push } }) => {
+  const onButtonClick = () => push('/pokedex');
+
   return (
     <div className={styles.root}>
       <Header />
@@ -20,7 +23,7 @@ const HomePage: React.FC<HomePageProps> = () => {
             <b>Find</b> all your favorite <b>Pokemon</b>
           </Heading>
           <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
-          <Button onClick={() => {}}>See pokemons</Button>
+          <Button onClick={onButtonClick}>See pokemons</Button>
         </div>
 
         <div>
