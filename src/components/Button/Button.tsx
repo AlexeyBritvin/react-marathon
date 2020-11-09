e8bb348959d667b6558835de3050cb2f6a6b8cb7
Aleksey <krugombeda@gmail.com>
2020-11-09 11:44:58 +0300

    add color, size props to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import cn from 'classnames';
 
 import styles from './Button.module.scss';
 
 export interface ButtonProps {
+  size?: 'small' | 'normal' | 'fullwidth';
+  color?: 'green' | 'yellow';
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ size = 'normal', color = 'green', children, onClick }) => {
   return (
-    <button type="button" className={styles.root} onClick={onClick}>
+    <button type="button" className={cn(styles.root, styles[size], styles[color])} onClick={onClick}>
       {children}
     </button>
   );
